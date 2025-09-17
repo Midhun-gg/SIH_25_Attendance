8fe6514bf89a39354f721639b36373c1a8c982e9
Midhun-gg <midhungullapalli9@gmail.com>
2025-09-17 11:48:42 +0530

    Updated databases

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import sqlite3, io, cv2, numpy as np, shutil, os
 from pathlib import Path
 from PIL import Image
 import face_recognition
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify ["http://localhost:port"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ---------------- DATABASE ----------------
 conn = sqlite3.connect("attendance.db", check_same_thread=False)
